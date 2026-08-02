08b4c383a0d2bf8e69f5db2995b436b464e3fbd1
Ikshita <rikshita08@gmail.com>
2026-08-02 23:57:49 +0530

    Time: 3 ms (78%), Space: 46.6 MB (37.87%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,23 +1,44 @@
 class Solution {
     public int romanToInt(String s) {
-       Map<Character,Integer> map = Map.of(
-        'I',1,
-        'V',5,
-        'X', 10,
-        'L', 50,
-        'C', 100,
-        'D', 500,
-        'M', 1000
-       );
-       int res =0;
-       for(int i=0;i<s.length();i++){
-        int curr = map.get(s.charAt(i));
-        int next = (i+1 < s.length()) ? map.get(s.charAt(i+1)) : 0;
-        if(curr<next)
+    //    Map<Character,Integer> map = Map.of(
+    //     'I',1,
+    //     'V',5,
+    //     'X', 10,
+    //     'L', 50,
+    //     'C', 100,
+    //     'D', 500,
+    //     'M', 1000
+    //    );
+    //    int res =0;
+    //    for(int i=0;i<s.length();i++){
+    //     int curr = map.get(s.charAt(i));
+    //     int next = (i+1 < s.length()) ? map.get(s.charAt(i+1)) : 0;
+    //     if(curr<next)
+    //         res -= curr;
+    //     else
+    //         res += curr;
+    //    } 
+    //    return res;
+
+    int[] arr = new int[128];
+    arr['I'] = 1;
+    arr['V'] = 5;
+    arr['X'] = 10;
+    arr['L'] = 50;
+    arr['C'] = 100;
+    arr['D'] = 500;
+    arr['M'] = 1000;
+
+    int res =0;
+    int prev =0;
+    for(int i = s.length()-1;i>=0;i--){
+        int curr = arr[s.charAt(i)];
+        if(curr < prev)
             res -= curr;
         else
             res += curr;
-       } 
-       return res;
+        prev = curr;
+    }
+    return res;
     }
 }
